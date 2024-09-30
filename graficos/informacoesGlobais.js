8cdb9112c10f8699dfffdc7bdbb40385e132c1a8
LARISSABRIXNER <107216553+LARISSABRIXNER@users.noreply.github.com>
2024-09-30 11:39:11 -0300

    30.09
    aula 7

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,6 +1,16 @@
-const url= 'https://github.com/LARISSABRIXNER/ciencia-de-dados/blame/main/README.md'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function name(params) {
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
 
-    
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasNoMundo} bilhoes</span> de pessoas e que aproximadamente <span> ${pessoasConectadas} bilhoes</span> estão conectadas a algumas redes sociais e passam em média <span> ${dados.tempo_medio} </span> horas conectadas.`
+    const Container = document.getElementById('graficos-container');
+    Container.appendChild(paragrafo);
 }
+
+vizualizarInformacoesGlobais()
