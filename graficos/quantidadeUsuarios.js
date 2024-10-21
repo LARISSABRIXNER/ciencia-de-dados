b9251d2af20ac34247c4dde469360a3c2ee5f9fa
LARISSABRIXNER <107216553+LARISSABRIXNER@users.noreply.github.com>
2024-10-21 11:24:13 -0300

    21.10.24
    aula 1

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -19,10 +19,37 @@ color: getCSS('--primary-color')
 ]
 
 const layout = {
-plot_bgcolor: getCSS('--bg-color'),
-paper_bgcolor: getCSS('--bg-color')
-
-}
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+        text: 'Redes sociais que os usuarios mais gostam',
+        x:0,
+        font: {
+            color: getCSS('--primary-color'),
+            family: getCSS('--font'),
+            size: 30
+        }
+    }, 
+    xaxis: {
+        tickfont: tickConfig,
+        title: {
+            text: 'nome das redes sociais',
+            font: {
+                color: getCSS('--secondary-color')
+            }
+        }
+    },
+    yaxis: {
+        tickfont: tickConfig,
+        title: {
+            text: 'bilhões de usuários ativos',
+            font: {
+                color: getCSS('--secondary-color')
+            }
+        }
+    }
+    
+    }
 
 const grafico = document.createElement('div')
 grafico.className = 'grafico'
